9357a35778032fd0cbad0a2eb77f86c70dafb21a
Fahad <82506574+FahadAlkhayyal@users.noreply.github.com>
2021-07-29 16:48:13 +0300

    add: add countries section

---
+++ src/components/Countries/Countries.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="register" id="countries">
+        <div class="container countriesShape">
+            <h2 class="title mr-md-4 aos-init aos-animate" data-aos="fade-down" data-aos-duration="200">
+                دول المرحلة الأولى التي يعمل فيها تطبيق توكلنا
+            </h2>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class=" d-flex flex-wrap">
+                        <div class="countryCard">
+                            <div class="countryFlag" style="background-image: url(images/flags/SaudiArabia.png);"></div>
+                            <p class="mt-2">المملكة العربية السعودية</p>
+                        </div>
+                        <div class="countryCard">
+                            <div class="countryFlag" style="background-image: url(images/flags/uae.png);"></div>
+                            <p class="mt-2">الإمارات العربية المتحدة</p>
+                        </div>
+                        <div class="countryCard">
+                            <div class="countryFlag" style="background-image: url(images/flags/Bahrain.svg);"></div>
+                            <p class="mt-2">البحرين</p>
+                        </div>
+                        <div class="countryCard">
+                            <div class="countryFlag" style="background-image: url(images/flags/Kuwait.png); "></div>
+                            <p class="mt-2">الكويت</p>
+                        </div>
+                        
+                        <div class="countryCard">
+                            <div class="countryFlag" style="background-image: url(images/flags/qatar.png);"></div>
+                            <p class="mt-2">دولة قطر</p>
+                        </div>
+                        <div class="countryCard">
+                            <div class="countryFlag" style="background-image: url(images/flags/Oman.png);"></div>
+                            <p class="mt-2">سلطنة عمان</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                        <a href="countries.html" class="btn seeMoreBtn rounded-pill mx-md-3 my-4"> 
+                            <span>عرض المزيد</span>
+                            <i class="fas fa-chevron-left mr-2"></i>
+                            </a>
+                </div>
+                </div>
+        </div>
+            
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
