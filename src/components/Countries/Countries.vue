6b014ef94eda1335ea80b22711c6cf7417f8d678
Fahad <82506574+FahadAlkhayyal@users.noreply.github.com>
2021-07-29 19:55:34 +0300

    add: add countries section

--- src/components/Countries/Countries.vue
+++ src/components/Countries/Countries.vue
@@ -1,48 +1,39 @@
 <template>
-    <section class="register" id="countries">
-        <div class="container countriesShape">
-            <h2 class="title mr-md-4 aos-init aos-animate" data-aos="fade-down" data-aos-duration="200">
-                دول المرحلة الأولى التي يعمل فيها تطبيق توكلنا
-            </h2>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class=" d-flex flex-wrap">
-                        <div class="countryCard">
-                            <div class="countryFlag" style="background-image: url(images/flags/SaudiArabia.png);"></div>
-                            <p class="mt-2">المملكة العربية السعودية</p>
-                        </div>
-                        <div class="countryCard">
-                            <div class="countryFlag" style="background-image: url(images/flags/uae.png);"></div>
-                            <p class="mt-2">الإمارات العربية المتحدة</p>
-                        </div>
-                        <div class="countryCard">
-                            <div class="countryFlag" style="background-image: url(images/flags/Bahrain.svg);"></div>
-                            <p class="mt-2">البحرين</p>
-                        </div>
-                        <div class="countryCard">
-                            <div class="countryFlag" style="background-image: url(images/flags/Kuwait.png); "></div>
-                            <p class="mt-2">الكويت</p>
-                        </div>
-                        
-                        <div class="countryCard">
-                            <div class="countryFlag" style="background-image: url(images/flags/qatar.png);"></div>
-                            <p class="mt-2">دولة قطر</p>
-                        </div>
-                        <div class="countryCard">
-                            <div class="countryFlag" style="background-image: url(images/flags/Oman.png);"></div>
-                            <p class="mt-2">سلطنة عمان</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                        <a href="countries.html" class="btn seeMoreBtn rounded-pill mx-md-3 my-4"> 
-                            <span>عرض المزيد</span>
-                            <i class="fas fa-chevron-left mr-2"></i>
-                            </a>
-                </div>
-                </div>
-        </div>
+    <section class="countries_section">
+        <h1 class="countries_title">دول المرحلة الأولى التي يعمل فيها تطبيق توكلنا </h1>
+        <div class="countries_container">
+            <div class="countries_countryCard">
+                <div class="countries_countryFlag" style="background-image: url(images/flags/SaudiArabia.png);"></div>
+                <p class="countries_countryName">المملكة العربية السعودية</p>
+            </div>
+            <div class="countries_countryCard">
+                <div class="countries_countryFlag" style="background-image: url(images/flags/uae.png);"></div>
+                <p class="countries_countryName">الإمارات العربية المتحدة</p>
+            </div>
+            <div class="countries_countryCard">
+                <div class="countries_countryFlag" style="background-image: url(images/flags/Bahrain.svg);"></div>
+                <p class="countries_countryName">البحرين</p>
+            </div>
+            <div class="countries_countryCard">
+                <div class="countries_countryFlag" style="background-image: url(images/flags/Kuwait.png); "></div>
+                <p class="countries_countryName">الكويت</p>
+            </div>
             
+            <div class="countries_countryCard">
+                <div class="countries_countryFlag" style="background-image: url(images/flags/qatar.png);"></div>
+                <p class="countries_countryName">دولة قطر</p>
+            </div>
+            <div class="countries_countryCard">
+                <div class="countries_countryFlag" style="background-image: url(images/flags/Oman.png);"></div>
+                <p class="countries_countryName">سلطنة عمان</p>
+            </div>
+        </div>
+        <div class="countries_buttonContainer">
+                <a href="countries.html" class="countries_button"> 
+                    <span>عرض المزيد</span>
+                    <!-- <i class="fas fa-chevron-left mr-2"></i> -->
+                </a>
+        </div>
     </section>
 </template>
 
@@ -51,5 +42,91 @@
 </script>
 
 <style>
+
+.countries_section {
+    direction: rtl;
+    padding: 100px;
+}
+
+.countries_title {
+    color: #1FA99A;
+    font-size: 40px;
+    margin-bottom: 90px;
+    position: relative;
+}
+
+.countries_countryFlag {
+    background-repeat:no-repeat;
+    background-size:contain;
+    height:200px;width:1200px;
+}
+
+.countries_container {
+    display: flex !important;
+    flex-wrap: wrap !important;
+}
+
+.countries_countryCard {
+    width: 130px;
+    max-width: 130px;
+    text-align: center;
+    margin: 10px;
+}
+
+.countries_countryFlag {
+    display: block;
+    width: 55px;
+    height: 55px;
+    border-radius: 50%;
+    background-size: cover;
+    background-position-x: center;
+    margin: 0 auto;
+    border: 1px solid #f4f4f4;
+}
+
+.countries_countryName {
+    margin-top: .5rem !important;
+}
+
+.countries_button {
+    background-color: #1FA99A;
+    background: rgb(8,100,79);
+    background: linear-gradient(47deg, rgba(8,100,79,1) 0%, rgba(20,159,144,1) 100%);
+    color: white;
+    padding: 8px 30px;
+    transition: all 0.2 ease-in-out;
+
+    margin-left: 1rem !important;
+    margin-right: 1rem !important;
+    margin-bottom: 1.5rem !important;
+    border-radius: 50rem !important;
+    display: inline-block;
+font-weight: 400;
+color: #212529;
+text-align: center;
+vertical-align: middle;
+-webkit-user-select: none;
+-moz-user-select: none;
+-ms-user-select: none;
+user-select: none;
+background-color: transparent;
+border: 1px solid transparent;
+padding: .375rem .75rem;
+font-size: 1rem;
+line-height: 1.5;
+border-radius: .25rem;
+transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+
+text-decoration: none;
+outline: none;
+
+color: white
+
+}
+
+.countries_button:hover {
+    text-decoration: none;
+outline: none;
+}
 
 </style>
