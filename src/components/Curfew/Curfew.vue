3e6c013d59753988b6676837fbb2d827b64b5bbb
maryam-alraddadi <maryam.alraddadi@tuwaiq.edu.sa>
2021-07-29 19:54:51 +0300

    add: curfew section data and icons

---
+++ src/components/Curfew/Curfew.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-for="service in services" :key="service.serviceId">
+    <ServiceCard :data="service" />
+  </div>
+</template>
+
+<script>
+import ServiceCard from "../CautiouslyWeReturn/ServiceCard.vue";
+import data from "./data";
+export default {
+  name: "Curfew",
+  components: {
+    ServiceCard,
+  },
+  data() {
+    return {
+      services: data,
+    };
+  },
+};
+</script>
